fb5e30ccba8e5764aa057e5a5505802986389044
Subrata Saha <msdsubrata9@gmail.com>
2024-12-17 12:52:40 +0530

    Changes the arrow

--- src/components/FaqCard.js
+++ src/components/FaqCard.js
@@ -9,7 +9,9 @@ function FaqCard({ faq, isOpenByDefault }) {
   return (
     <div className="bg-green-500 m-4 p-4 text-center">
       <div className="cursor-pointer" onClick={handleClick}>
-        <span className="text-5xl text-black m-2">{showCard ? "V" : ">"}</span>
+        <span className="text-4xl text-black m-2">
+          {showCard ? "🔽" : "▶️"}
+        </span>
         <span className="text-3xl text-red-500">{faq.question}</span>
       </div>
       {showCard && <div className="text-2xl text-blue-500">{faq.answer}</div>}
